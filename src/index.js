d05cdfcb8420f3862685f78b58f333aec19d0b3e
Sergio Eduardo Vega Guzmán <48001421+classic-mathematician@users.noreply.github.com>
2022-05-29 22:43:38 -0500

    visualización de formularios
    Formulario mensual, semanal y diario.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import FormularioDiario from './formularios';
+import FormularioSemanal from './formularios';
+import FormularioMensual from './formularios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <FormularioSemanal />
-    
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-import Formulario from './Formulario';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<div className="contenedor">
-			<Formulario />
+			<FormularioSemanal />
 		</div>
 	</React.StrictMode>,
 	document.getElementById('root')
